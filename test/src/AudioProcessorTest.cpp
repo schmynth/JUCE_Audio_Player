327138d6d6d3258752fde103a755536077e2a190
schmynth <service-noreply@gmx.de>
2025-03-03 10:11:13 +0100

    add google test

---
+++ test/src/AudioProcessorTest.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+#include <PluginProcessor.h>
+
+namespace audio_plugin_test {
+    TEST(AudioPluginAudioProcessor, Foo) {
+        AudioPluginAudioProcessor processor{};
+        ASSERT_FALSE(true);
+    }
+}
